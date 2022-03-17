578482aee6c7eddac7531fcb3300cd81a0a31b32
Hubertcior <hradziwinski@gmail.com>
2022-03-18 00:06:22 +0100

    1.3

--- app.js
+++ app.js
@@ -10,10 +10,12 @@ const checkRow = () =>{
     if(table[0] === 1 && table[1] === 1 && table[2] === 1 || table[3] === 1 && table[4] === 1 && table[5] === 1 || table[6] === 1 && table[7] === 1 && table[8] === 1){
         canPlay = !canPlay;
         document.querySelector('h1').innerHTML= "X win";
+        console.log("X win");
     }
     else if(table[0] === 2 && table[1] === 2 && table[2] === 2 || table[3] === 2 && table[4] === 2 && table[5] === 2 || table[6] === 2 && table[7] === 2 && table[8] === 2){
         document.querySelector('h1').innerHTML= "O win";
         canPlay = !canPlay;
+        console.log("O win");
     }
 }
 
@@ -21,10 +23,12 @@ const checkColumn = () =>{
     if(table[0] === 1 && table[3] === 1 && table[6] === 1 || table[1] === 1 && table[4] === 1 && table[7] === 1 || table[2] === 1 && table[5] === 1 && table[8] === 1){
         canPlay = !canPlay;
         document.querySelector('h1').innerHTML= "X win";
+        console.log("X win");
     }
     else if(table[0] === 2 && table[3] === 2 && table[6] === 2 || table[1] === 2 && table[4] === 2 && table[7] === 2 || table[2] === 2 && table[5] === 2 && table[8] === 2){
         document.querySelector('h1').innerHTML= "O win";
         canPlay = !canPlay;
+        console.log("O win");
     }
 }
 
@@ -32,10 +36,12 @@ const checkBias = () =>{
     if(table[0] === 1 && table[4] === 1 && table[8] === 1 || table[2] === 1 && table[4] === 1 && table[6] === 1){
         canPlay = !canPlay;
         document.querySelector('h1').innerHTML= "X win";
+        console.log("X win");
     }
     else if(table[0] === 2 && table[4] === 2 && table[8] === 2 || table[2] === 2 && table[4] === 2 && table[6] === 2){
         document.querySelector('h1').innerHTML= "O win";
         canPlay = !canPlay;
+        console.log("O win");
     }
 }
 
@@ -43,6 +49,7 @@ const checkDraw = () =>{
     if(table[0] != 0 && table[1] != 0 && table[2] != 0 && table[3] != 0 && table[4] != 0 && table[5] != 0 && table[6] != 0 && table[7] != 0 && table[8] != 0){
         document.querySelector('h1').innerHTML= "Draw";
         canPlay = !canPlay;
+        console.log("draw");
     }
 }
 
