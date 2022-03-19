43dcf2bac755c81924d8f30be2c195a079903ca5
Hubertcior <hradziwinski@gmail.com>
2022-03-19 12:42:08 +0100

    1.4v
    Logic update

--- app.js
+++ app.js
@@ -1,62 +1,96 @@
 const fields = document.querySelectorAll('.square');
-
-let flag = true;
+let changePlayer = true;
 let canPlay = true;
-let table = [0,0,0,
+let tableX= [0,0,0,
                    0,0,0,
                    0,0,0];
-
+let tableO= [0,0,0,
+                   0,0,0,
+                   0,0,0];
 let games = 0;
 let oWins= 0;
 let xWins= 0;
 let draws= 0;
+let moves = 0;
 
-const check = () =>{
-    if(table[0] === 1 && table[1] === 1 && table[2] === 1 || table[3] === 1 && table[4] === 1 && table[5] === 1 || table[6] === 1 && table[7] === 1 && table[8] === 1){
+const checkXWin = () => {
+    for(let i = 0;i<tableX.length;i+=3){
+        if(tableX[i] ==1 && tableX[i+1] == 1 && tableX[i+2] ==1){
+            canPlay = !canPlay;
+            document.querySelector('h1').innerHTML= "X Win";
+            games++;
+            xWins++;
+        }
+    }
+    for(let i = 0;i<3;i++){
+        if(tableX[i] ==1 && tableX[i+3] == 1 && tableX[i+6] ==1){
+            canPlay = !canPlay;
+            document.querySelector('h1').innerHTML= "X Win";
+            games++;
+            xWins++;
+        }
+    }
+    if(tableX[0] ==1 && tableX[4] == 1 && tableX[8] ==1){
         canPlay = !canPlay;
-        document.querySelector('h1').innerHTML= "X win";
+        document.querySelector('h1').innerHTML= "X Win";
+        games++;
         xWins++;
-        games++;
     }
-    else if(table[0] === 2 && table[1] === 2 && table[2] === 2 || table[3] === 2 && table[4] === 2 && table[5] === 2 || table[6] === 2 && table[7] === 2 && table[8] === 2){
-        document.querySelector('h1').innerHTML= "O win";
+
+    if(tableX[2] ==1 && tableX[4] == 1 && tableX[6] ==1){
         canPlay = !canPlay;
-        oWins++;
+        document.querySelector('h1').innerHTML= "X Win";
         games++;
-    }
-    else if(table[0] === 1 && table[3] === 1 && table[6] === 1 || table[1] === 1 && table[4] === 1 && table[7] === 1 || table[2] === 1 && table[5] === 1 && table[8] === 1){
-        canPlay = !canPlay;
-        document.querySelector('h1').innerHTML= "X win";
         xWins++;
-        games++;
     }
-    else if(table[0] === 2 && table[3] === 2 && table[6] === 2 || table[1] === 2 && table[4] === 2 && table[7] === 2 || table[2] === 2 && table[5] === 2 && table[8] === 2){
-        document.querySelector('h1').innerHTML= "O win";
+
+    if(moves === 9){
         canPlay = !canPlay;
-        oWins++;
-        games++;
-    }
-    else if(table[0] === 1 && table[4] === 1 && table[8] === 1 || table[2] === 1 && table[4] === 1 && table[6] === 1){
-        canPlay = !canPlay;
-        document.querySelector('h1').innerHTML= "X win";
-        xWins++;
-        games++;
-    }
-    else if(table[0] === 2 && table[4] === 2 && table[8] === 2 || table[2] === 2 && table[4] === 2 && table[6] === 2){
-        document.querySelector('h1').innerHTML= "O win";
-        canPlay = !canPlay;
-        oWins++;
-        games++;
-    }
-    else if(table[0] != 0 && table[1] != 0 && table[2] != 0 && table[3] != 0 && table[4] != 0 && table[5] != 0 && table[6] != 0 && table[7] != 0 && table[8] != 0){
         document.querySelector('h1').innerHTML= "Draw";
+        games++;
+        draws++;
+    }
+}
+
+const checkOWin = () => {
+    for(let i = 0;i<tableO.length;i+=3){
+        if(tableO[i] ==1 && tableO[i+1] == 1 && tableO[i+2] ==1){
+            canPlay = !canPlay;
+            document.querySelector('h1').innerHTML= "O Win";
+            games++;
+            oWins++;
+        }
+    }
+    for(let i = 0;i<3;i++){
+        if(tableO[i] ==1 && tableO[i+3] == 1 && tableO[i+6] ==1){
+            canPlay = !canPlay;
+            document.querySelector('h1').innerHTML= "O Win";
+            games++;
+            oWins++;
+        }
+    }
+    if(tableO[0] ==1 && tableO[4] == 1 && tableO[8] ==1){
         canPlay = !canPlay;
-       draws++;
-       games++;
+        document.querySelector('h1').innerHTML= "O Win";
+        games++;
+        oWins++;
     }
 
+    if(tableO[2] ==1 && tableO[4] == 1 && tableO[6] ==1){
+        canPlay = !canPlay;
+        document.querySelector('h1').innerHTML= "O Win";
+        games++;
+        oWins++;
+    }
+    if(moves === 9){
+        canPlay = !canPlay;
+        document.querySelector('h1').innerHTML= "Draw";
+        games++;
+        draws++;
+    }
 }
-const clearingImg = () =>{
+
+const clearImg = () =>{
     fields.forEach(field =>{
         if(field.childNodes[0] != undefined){
         field.removeChild(field.childNodes[0]);
@@ -64,23 +98,27 @@ const clearingImg = () =>{
     })
 }
 
-const clearingTable = () =>{
-    for(let i = 0; i<table.length; i++){
-        table[i] = 0;
+const clearTables = () =>{
+    for(let i = 0; i<tableX.length; i++){
+        tableX[i] = 0;
+    }
+    for(let i = 0; i<tableO.length; i++){
+        tableO[i] = 0;
     }
 }
 
-const clearingValue = () =>{
-    flag = true;
+const clearValue = () =>{
+    changePlayer = true;
     canPlay = true;
+    moves = 0;
     document.querySelector('h1').innerHTML= "";
     document.querySelector('h2').innerHTML= "";
 }
 
 const restart = () =>{
-    clearingTable();
-    clearingValue();
-    clearingImg();
+    clearTables();
+    clearValue();
+    clearImg();
     statsUpdate();
 }
 
@@ -96,31 +134,35 @@ statsUpdate();
 
 fields.forEach( element => {
     element.addEventListener('click', ()=>{
-        if(table[element.attributes[0].value]===0){
-        if(flag && canPlay){
+        if(tableX[element.attributes[0].value]===0 && tableO[element.attributes[0].value]===0){
+        if(changePlayer && canPlay){
         const text = document.createElement('h1');
         text.innerHTML = "X";
         text.style.fontSize = "80px";
         text.style.color = "red";
         element.appendChild(text);
-        flag = !flag;
-        table[element.attributes[0].value] =1;
-        check();
+        changePlayer = !changePlayer;
+        tableX[element.attributes[0].value] =1;
         document.querySelector('h2').innerHTML= "";
+        console.table(tableX);
+        moves++;
+        checkXWin();
         }
-        else if(!flag && canPlay){
+        else if(!changePlayer && canPlay){
         const text = document.createElement('h1');
         text.innerHTML = "O"
         text.style.fontSize = "80px";
         text.style.color = "green";
         element.appendChild(text);
-        flag = !flag;
-        table[element.attributes[0].value] =2;
-        check();
+        changePlayer = !changePlayer;
+        tableO[element.attributes[0].value] =1;
         document.querySelector('h2').innerHTML= "";
+        console.table(tableO);
+        moves++;
+        checkOWin();
         }
     }
-    else{
+    else if(canPlay){
         document.querySelector('h2').innerHTML= "wybierz dobre pole!";
     }
     });
